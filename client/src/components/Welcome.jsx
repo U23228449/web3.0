cff701b958742035aecacb230b34586cab0f0328
Ruben <u23228449@utp.edu.pe>
2024-10-24 20:54:25 -0500

    First Commit

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,48 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+        placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+);
+
+const Welcome = () => {
+    const connectWallet = () => {
+
+    }
+
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+                        Send Crypto <br /> across the world
+                    </h1>
+                    <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+                    </p>
+                    <button type="button"
+                        onClick={connectWallet}
+                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                        <p className="text-white text-base font-semibold">Connect Wallet</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Welcome;
