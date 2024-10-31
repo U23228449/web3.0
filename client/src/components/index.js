86bc42be7b205cacfbd5c5c85da45b6f0a6e0c98
Ruben Laptop Mac <u23228449@utp.edu.pe>
2024-10-30 19:14:11 -0500

    Comit 30 Octubre

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,4 @@ export {default as Navbar} from './Navbar';
 export {default as Footer} from './Footer';
 export {default as Welcome} from './Welcome';
 export {default as Transactions} from './Transactions';
-export {default as Services} from './Services';
 //Prueba de que funciona el git
